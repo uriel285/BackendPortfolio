b3df5ae2c2054537a54fc7144646b4c07fe1f0dd
Uriel Herrera <135197548+uriel285@users.noreply.github.com>
2023-11-23 01:07:01 -0300

    Update bd.js

--- bd.js
+++ bd.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 
 var pool = mysql.createPool({
+    waitForConnections: true,
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
-    port: process.env.MYSQL_PORT
 })
 
 pool.query = util.promisify(pool.query).bind(pool);

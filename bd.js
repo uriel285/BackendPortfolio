d6d6c5bfd51fbfefc3f3a04720579d0d75078ab9
Uriel Herrera <135197548+uriel285@users.noreply.github.com>
2023-11-23 00:50:30 -0300

    Update bd.js

--- bd.js
+++ bd.js
@@ -8,7 +8,8 @@ var pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    database: process.env.MYSQL_DB_NAME,
+    port: process.env.MYSQL_PORT
 })
 
 pool.query = util.promisify(pool.query).bind(pool);
